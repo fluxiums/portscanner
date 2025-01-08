dd6672dade5fdff4d03b9480959579aaa07719ed
fluxium <fluxiumwebdev@gmail.com>
2025-01-08 08:24:31 -0800

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,44 +1,36 @@
-import pyfiglet
-import sys
-import socket
-from datetime import datetime
+import socket # for connecting
+from colorama import init, Fore
 
-ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
-print(ascii_banner)
+# some colors
+init()
+GREEN = Fore.GREEN
+RESET = Fore.RESET
+GRAY = Fore.LIGHTBLACK_EX
 
-# Defining a target
-if len(sys.argv) == 2:
-	
-	# translate hostname to IPv4
-	target = socket.gethostbyname(sys.argv[1]) 
-else:
-	print("Invalid amount of Argument")
+def is_port_open(host, port):
+    """
+    determine whether `host` has the `port` open
+    """
+    # creates a new socket
+    s = socket.socket()
+    try:
+        # tries to connect to host using that port
+        s.connect((host, port))
+        # make timeout if you want it a little faster ( less accuracy )
+        s.settimeout(0.2)
+    except:
+        # cannot connect, port is closed
+        # return false
+        return False
+    else:
+        # the connection was established, port is open!
+        return True
 
-# Add Banner 
-print("-" * 50)
-print("Scanning Target: " + target)
-print("Scanning started at:" + str(datetime.now()))
-print("-" * 50)
-
-try:
-	
-	# will scan ports between 1 to 65,535
-	for port in range(1,65535):
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		socket.setdefaulttimeout(1)
-		
-		# returns an error indicator
-		result = s.connect_ex((target,port))
-		if result ==0:
-			print("Port {} is open".format(port))
-		s.close()
-		
-except KeyboardInterrupt:
-		print("\n Exiting Program !!!!")
-		sys.exit()
-except socket.gaierror:
-		print("\n Hostname Could Not Be Resolved !!!!")
-		sys.exit()
-except socket.error:
-		print("\ Server not responding !!!!")
-		sys.exit()
+# get the host from the user
+host = input("Enter the host:")
+# iterate over ports, from 1 to 1024
+for port in range(1, 1025):
+    if is_port_open(host, port):
+        print(f"{GREEN}[+] {host}:{port} is open      {RESET}")
+    else:
+        print(f"{GRAY}[!] {host}:{port} is closed    {RESET}", end="\r")
